48384ff0c6d149dd91d2133ae6f284f4c68d11a3
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2024-11-30 18:13:03 +0100

    🎂

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'tailwindcss'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+export default <Partial<Config>>{
+	theme: {
+		extend: {
+			colors: {
+				brand: {
+					50: '#fffeea',
+					100: '#fffac5',
+					200: '#fff685',
+					300: '#ffea46',
+					400: '#ffdb1b',
+					500: '#ffbb00',
+					600: '#e29000',
+					700: '#bb6502',
+					800: '#984e08',
+					900: '#7c400b',
+					950: '#482100',
+				},
+			}
+		}
+	}
+}
