c4e3d1dc20f34aae568d67f510a056c871ccd7bf
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2025-02-17 22:45:30 +0100

    Refactor: Move plan groups to nested route

--- pages/plan-groups/[group].vue
+++ pages/plan-groups/[group].vue
@@ -1,21 +1,18 @@
 <script setup lang="ts">
 import type { Database, Tables } from "~/database.types"
-import { useRoute } from "vue-router"
 import VueMarkdown from "vue-markdown-render"
 import type Options from "vue-markdown-render"
-import type { BreadcrumbLink } from "#ui/types"
 
 const options: typeof Options = {
 	html: true
 }
 
 const route = useRoute()
-// console.log(route.params.group)
 
 const supabase = useSupabaseClient<Database>()
 type PlanGroup = Tables<"test_plan_groups">
 
-const planGroup = ref<PlanGroup | null>(null)
+const planGroup = ref<PlanGroup>()
 
 // slug group is id
 async function getPlanGroup() {
@@ -30,44 +27,12 @@ async function getPlanGroup() {
 	planGroup.value = data[0]
 }
 
-onMounted(() => {
-	getPlanGroup()
-})
-
-const breadcrumbs = ref<BreadcrumbLink[]>([])
-watch(planGroup, () => {
-	if (planGroup.value) {
-		breadcrumbs.value = [
-			{
-				label: "Home",
-				to: "/",
-				icon: "i-lucide-home"
-			},
-			{
-				label: "Plan Groups",
-				to: "/plan-groups",
-				icon: "i-lucide-library-big"
-			},
-			{
-				label: planGroup.value.title || "",
-				to: "",
-				icon: "i-lucide-library-big"
-			}
-		]
-	}
-})
+await getPlanGroup()
 </script>
 
 <template>
 	<div class="flex flex-col lg:flex-row gap-3 w-full">
 		<div v-if="planGroup">
-			<div class="mb-4">
-				<UBreadcrumb :links="breadcrumbs">
-					<template #default="{ link, isActive }">
-						<div :class="{ 'font-bold': isActive }">{{ link.label }}</div>
-					</template>
-				</UBreadcrumb>
-			</div>
 			<h1 class="text-4xl font-bold text-primary mb-8">
 				{{ planGroup?.title }}
 			</h1>
