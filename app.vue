2ba7d7b825e6f15593950022a2caa4487de572d3
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2025-02-17 22:30:28 +0100

    Style: Refactor styles to actual CSS file

--- app.vue
+++ app.vue
@@ -3,17 +3,3 @@
 		<NuxtPage />
 	</NuxtLayout>
 </template>
-
-<style>
-.dark {
-	--color-primary-DEFAULT: var(--color-primary-500);
-}
-
-.dark\:text-primary-400:is(.dark *) {
-	color: rgb(var(--color-primary-500) / var(--tw-text-opacity, 1));
-}
-
-.dark\:disabled\:text-primary-400:disabled:is(.dark *) {
-	color: rgb(var(--color-gray-500) / var(--tw-text-opacity, 1));
-}
-</style>
