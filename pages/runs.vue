1d336e5467b8c84d5bda8ae4fe79b9336f29deed
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2025-02-23 23:07:25 +0100

    WIP: Push to finish at school

--- pages/runs.vue
+++ pages/runs.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-useHead({
-	title: `Test Runs | Test Suite`
-})
-</script>
-
 <template>
-	<div>runs</div>
+	<NuxtPage />
 </template>
---
+++ pages/runs/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { Database, Tables } from "~/database.types"
+
+const supabase = useSupabaseClient<Database>()
+
+useHead({
+	title: `Test Runs | Test Suite`
+})
+</script>
+
+<template>
+	<div>runs</div>
+</template>
