63289dfdc936ac81822984ef0be22c9f01311408
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2025-12-06 13:19:48 +0100

    Layout: Create page wrapper component

---
+++ app/components/PageWrapper.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+const props = defineProps<{
+	breadcrumbs?: string
+	title?: string
+}>()
+</script>
+
+<template>
+	<div class="flex flex-col gap-y-3">
+		<div class="flex items-center justify-between gap-3 w-full">
+			<div class="flex items-center gap-2">
+				<slot name="breadcrumbs-leading" />
+				<div v-if="breadcrumbs" class="flex gap-4 items-center">
+					<div class="font-semibold text-primary-500">
+						{{ breadcrumbs }}
+					</div>
+				</div>
+				<slot name="breadcrumbs" />
+			</div>
+			<slot name="breadcrumbs-trailing" />
+		</div>
+		<div class="flex items-center justify-between gap-3 w-full">
+			<div class="flex items-center gap-2">
+				<slot name="title-leading" />
+				<h1 v-if="title" class="text-5xl font-bold mb-4">
+					{{ props.title }}
+				</h1>
+				<slot name="title" />
+			</div>
+			<slot name="title-trailing" />
+		</div>
+		<USeparator />
+		<slot name="content" />
+	</div>
+</template>
