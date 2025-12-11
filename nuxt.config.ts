dce6275b30099ca64d372286d5d5074bb2597c7a
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2025-12-11 22:34:26 +0100

    Module: Install Pinia for Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxtjs/supabase"],
+	modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxtjs/supabase", "@pinia/nuxt"],
 	compatibilityDate: "2024-11-30",
 	supabase: {
 		redirectOptions: {
