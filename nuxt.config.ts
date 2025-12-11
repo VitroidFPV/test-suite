dcb21a6b32cc78c3f18d60f709e02ad0007c96bc
VitroidFPV <76877124+VitroidFPV@users.noreply.github.com>
2025-12-11 23:05:13 +0100

    Module: Add persisted state Pinia plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxtjs/supabase", "@pinia/nuxt"],
+	modules: [
+		"@nuxt/ui",
+		"@nuxt/eslint",
+		"@nuxtjs/supabase",
+		"@pinia/nuxt",
+		"pinia-plugin-persistedstate/nuxt"
+	],
 	compatibilityDate: "2024-11-30",
 	supabase: {
 		redirectOptions: {
